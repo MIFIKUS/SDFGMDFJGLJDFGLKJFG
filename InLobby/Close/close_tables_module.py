7fc2f60c13a3e55542bc5cb94035f4ef92c436b9
MIFIKUS <mifikus322@gmail.com>
2025-08-16 19:57:08 +0300

    Исправлено форматирование кода в функции run для улучшения читаемости и структурированности, добавлены пробелы между логическими блоками.

--- InLobby/Close/close_tables_module.py
+++ InLobby/Close/close_tables_module.py
@@ -16,10 +16,10 @@ def run():
                 if tables.table_closed():
                     tournament_id = tables.get_table_tournament_id(table_window)
                     table_num = tables.get_table_num(table_window)
-    
+
                     tables_proceses.close_table(table_window)
-    
-                rename.set_file_to_complete(tournament_id, table_num)
+
+                    rename.set_file_to_complete(tournament_id, table_num)
             except:
                 print("Ошибка закрытия стола")
                 traceback.print_exc()
